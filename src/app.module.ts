e26ae89a9e619405324901c07c229d18e9049079
Soiman Al Fath <117827510+soimalfath@users.noreply.github.com>
2024-03-15 20:40:58 +0700

    add module weather

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { WeatherService } from './weather/weather.service';
-import { WeatherController } from './weather/weather.controller';
+
 import { ConfigModule } from '@nestjs/config';
+import { WeatherModule } from './weather/weather.module';
 
 @Module({
-  imports: [ConfigModule.forRoot()],
-  controllers: [AppController, WeatherController],
-  providers: [AppService, WeatherService],
+  imports: [ConfigModule.forRoot(), WeatherModule],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
